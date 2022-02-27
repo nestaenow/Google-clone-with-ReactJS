72e4c51d289752e182108f762af1c1516ea923f1
Nesta <nestaenow@gmail.com>
2022-02-27 18:31:24 +0100

    components

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <div className={darkTheme ? 'dark' : ''}>
       <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
-        <Navbar />
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
         <Footer />
         <Routes />
       </div>
